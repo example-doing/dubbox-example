2e68a71bba3315125db707d5c27b5f2cadf6e7f2
879229629 <zg879229629@gmail.com>
2017-06-12 16:12:43 +0800

    调整

--- dubbox-api/src/main/java/com/zenggang/model/User.java
+++ dubbox-api/src/main/java/com/zenggang/model/User.java
@@ -5,10 +5,12 @@ import java.io.Serializable;
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
-
     private int id;
     private String name;
     private int age;
+
+    public User() {
+    }
 
     public User(int id, String name, int age) {
         this.id = id;
